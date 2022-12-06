d74c2293e63b81141600962aa98dfc4bf4f6e8bd
AndyBarnard <barnardandy1@gmail.com>
2022-12-06 11:18:59 +0700

    cleanUpResponseLegs

---
+++ packages/js/src/plugins/rfqModule/operations/cleanUpResponseLegs.ts
@@ -0,0 +1,137 @@
+import { createCleanUpResponseLegsInstruction } from '@convergence-rfq/rfq';
+import { PublicKey } from '@solana/web3.js';
+import { SendAndConfirmTransactionResponse } from '../../rpcModule';
+import { Convergence } from '@/Convergence';
+import {
+  Operation,
+  OperationHandler,
+  OperationScope,
+  useOperation,
+} from '@/types';
+import { TransactionBuilder, TransactionBuilderOptions } from '@/utils';
+
+const Key = 'CleanUpResponseLegsOperation' as const;
+
+/**
+ * Cleans up Legs for a Response
+ *
+ * ```ts
+ * await convergence
+ *   .rfqs()
+ *   .cleanUpResponseLegs({ address };
+ * ```
+ *
+ * @group Operations
+ * @category Constructors
+ */
+export const cleanUpResponseLegsOperation =
+  useOperation<CleanUpResponseLegsOperation>(Key);
+
+/**
+ * @group Operations
+ * @category Types
+ */
+export type CleanUpResponseLegsOperation = Operation<
+  typeof Key,
+  CleanUpResponseLegsInput,
+  CleanUpResponseLegsOutput
+>;
+
+/**
+ * @group Operations
+ * @category Inputs
+ */
+export type CleanUpResponseLegsInput = {
+  /**
+   * The address of the protocol
+   */
+  protocol: PublicKey;
+  /** The address of the Rfq account */
+  rfq: PublicKey;
+  /** The address of the Reponse account */
+  response: PublicKey;
+
+  /*
+   * Args
+   */
+
+  legAmountToClear: number;
+};
+
+/**
+ * @group Operations
+ * @category Outputs
+ */
+export type CleanUpResponseLegsOutput = {
+  /** The blockchain response from sending and confirming the transaction. */
+  response: SendAndConfirmTransactionResponse;
+};
+
+/**
+ * @group Operations
+ * @category Handlers
+ */
+export const cleanUpResponseLegsOperationHandler: OperationHandler<CleanUpResponseLegsOperation> =
+  {
+    handle: async (
+      operation: CleanUpResponseLegsOperation,
+      convergence: Convergence,
+      scope: OperationScope
+    ): Promise<CleanUpResponseLegsOutput> => {
+      scope.throwIfCanceled();
+
+      return cleanUpResponseLegsBuilder(
+        convergence,
+        operation.input,
+        scope
+      ).sendAndConfirm(convergence, scope.confirmOptions);
+    },
+  };
+
+/**
+ * @group Transaction Builders
+ * @category Inputs
+ */
+export type CancelResponseBuilderParams = CleanUpResponseLegsInput;
+
+/**
+ * Cancels an existing Response.
+ *
+ * ```ts
+ * const transactionBuilder = convergence
+ *   .rfqs()
+ *   .builders()
+ *   .cleanUpResponseLegs({ address });
+ * ```
+ *
+ * @group Transaction Builders
+ * @category Constructors
+ */
+export const cleanUpResponseLegsBuilder = (
+  convergence: Convergence,
+  params: CancelResponseBuilderParams,
+  options: TransactionBuilderOptions = {}
+): TransactionBuilder => {
+  const { programs, payer = convergence.rpc().getDefaultFeePayer() } = options;
+  const { protocol, rfq, response, legAmountToClear } = params;
+
+  const rfqProgram = convergence.programs().getRfq(programs);
+
+  return TransactionBuilder.make()
+    .setFeePayer(payer)
+    .add({
+      instruction: createCleanUpResponseLegsInstruction(
+        {
+          protocol,
+          rfq,
+          response,
+        },
+        {
+          legAmountToClear,
+        },
+        rfqProgram.address
+      ),
+      signers: [payer],
+      key: 'cleanUpResponseLegs',
+    });
+};
