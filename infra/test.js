417d2b1ee363698eab3cf834881425b8866ff10a
Pindaroso <me@pindaroso.com>
2023-01-06 17:12:17 -0500

    Fixed CI/CD tests

--- infra/test.js
+++ infra/test.js
@@ -2,22 +2,14 @@ const { spawn } = require('child_process');
 
 process.env.NX_TASKS_RUNNER_DYNAMIC_OUTPUT = false;
 
-const testAll = new Promise((resolve) => {
-    const child = spawn("yarn", ["test:all"], {
-        stdio: [process.stdin, process.stdout, process.stderr]
-    });
-    resolve(child);
-});
-
 const solanaLogs = new Promise((resolve) => {
-    // TODO: Find a better way to wait for the test validator to start
-    const x = setTimeout(() => {
+    // Logs are not available immediately after starting the test validator
+    setTimeout(() => {
         const child = spawn("solana", ["logs", "--url", "localhost"], {
             stdio: [process.stdin, process.stdout, process.stderr]
         });
-        return child;
-    }, 2_000);
-    resolve(x)
+        resolve(child);
+    }, 1_000);
 });
 
 const solanaTestValidator = new Promise((resolve) => {
@@ -28,7 +20,17 @@ const solanaTestValidator = new Promise((resolve) => {
 });
 
 async function main() {
-    await Promise.all([testAll, solanaLogs, solanaTestValidator]);
+    await Promise.all([solanaTestValidator, solanaLogs]);
+
+    const child = spawn("yarn", ["test:all"], {
+        stdio: [process.stdin, process.stdout, process.stderr]
+    });
+    
+    child.on("close", code => {
+        if (code === 0) {
+            process.exit(0);
+        }
+    });
 }
 
 main().then().catch(console.error);
