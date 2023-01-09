ec0d04b5c6bb423c5e66ae8d1d6426de7297516a
Pindaroso <me@pindaroso.com>
2023-01-09 12:29:00 -0500

    Stub

---
+++ packages/js/src/plugins/psyoptionsAmericanInstrumentModule/plugin.ts
@@ -0,0 +1,28 @@
+import { ProgramClient } from '../programModule';
+import { PSYOPTIONS_AMERICAN_INSTRUMENT_PROGRAM_ID } from './types';
+import { ConvergencePlugin, Program } from '@/types';
+import type { Convergence } from '@/Convergence';
+
+/** @group Plugins */
+export const psyoptionsAmericanInstrumentModule = (): ConvergencePlugin => ({
+  install(convergence: Convergence) {
+    const psyoptionsAmericanInstrumentProgram = {
+      name: 'PsyoptionsAmericanInstrumentProgram',
+      address: PSYOPTIONS_AMERICAN_INSTRUMENT_PROGRAM_ID,
+    };
+    convergence.programs().register(psyoptionsAmericanInstrumentProgram);
+
+    convergence.programs().getPsyoptionsAmericanInstrument = function (
+      this: ProgramClient,
+      programs?: Program[]
+    ) {
+      return this.get(psyoptionsAmericanInstrumentProgram.name, programs);
+    };
+  },
+});
+
+declare module '../programModule/ProgramClient' {
+  interface ProgramClient {
+    getPsyoptionsAmericanInstrument(programs?: Program[]): Program;
+  }
+}
