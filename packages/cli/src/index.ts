5a746c0f07858093c7b320b2f67348b89af788da
Pindaroso <me@pindaroso.com>
2023-01-23 18:04:34 -0500

    Added create wallet method

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -7,12 +7,12 @@ import {
   PublicKey,
   LAMPORTS_PER_SOL,
 } from '@solana/web3.js';
-import { Convergence, keypairIdentity } from '@convergence-rfq/sdk';
+import { Convergence, keypairIdentity, token } from '@convergence-rfq/sdk';
 
 type Options = any;
 
-const DEFAULT_RPC_ENDPOINT = 'http://127.0.0.1:8899';
 const DEFAULT_KEYPAIR_FILE = '/Users/pindaroso/.config/solana/dao.json';
+const DEFAULT_RPC_ENDPOINT = 'http://127.0.0.1:8899';
 
 // ACTIONS
 
@@ -40,6 +40,30 @@ const createMint = async (options: any) => {
   console.log('Success!');
 };
 
+const createWallet = async (options: any) => {
+  console.log('Creating wallet...');
+  const cvg = await createCvg(options);
+  const { token: wallet } = await cvg.tokens().createToken({
+    mint: new PublicKey(options.mint),
+    owner: new PublicKey(options.owner),
+  });
+  console.log('Address:', wallet.address.toString());
+  console.log('Success!');
+};
+
+const mintTo = async (options: any) => {
+  console.log('Minting to...');
+  const cvg = await createCvg(options);
+  const user = cvg.rpc().getDefaultFeePayer();
+  await cvg.tokens().mint({
+    mintAddress: new PublicKey(options.mint),
+    amount: token(options.amount),
+    toToken: new PublicKey(options.wallet),
+    mintAuthority: user.publicKey,
+  });
+  console.log('Success!');
+};
+
 const initializeProtocol = async (options: Options) => {
   console.log('Initializing protocol...');
   const cvg = await createCvg(options);
@@ -53,6 +77,9 @@ const initializeProtocol = async (options: Options) => {
 const createCvg = async (options: Options): Promise<Convergence> => {
   const secret = JSON.parse(readFileSync(options.keypairFile, 'utf8'));
   const user = Keypair.fromSecretKey(new Uint8Array(secret));
+  if (options.verbose) {
+    console.log('User:', user.publicKey.toString());
+  }
   const connection = new Connection(options.rpcEndpoint, {
     commitment: 'confirmed',
   });
@@ -64,6 +91,7 @@ const createCvg = async (options: Options): Promise<Convergence> => {
 const addDefaultArgs = (cmd: any) => {
   cmd.option('--rpc-endpoint <value>', 'RPC endpoint', DEFAULT_RPC_ENDPOINT);
   cmd.option('--keypair-file <value>', 'Keypair file', DEFAULT_KEYPAIR_FILE);
+  cmd.option('--verbose <value>', 'Verbose', false);
   return cmd;
 };
 
@@ -77,22 +105,37 @@ const airdropCmd = program
   .description('Airdrops SOL to the current user')
   .option('--amount <value>', 'Amount to airdrop in SOL')
   .action(airdrop);
-const setupMintsCmd = program
+const createMintCmd = program
   .command('create-mint')
   .description('Create mint')
   .option('--decimals <value>', 'Decimals')
   .action(createMint);
+const createWalletCmd = program
+  .command('create-wallet')
+  .description('Create wallet')
+  .option('--owner <value>', 'Owner address')
+  .option('--mint <value>', 'Mint address')
+  .action(createWallet);
+const mintToCmd = program
+  .command('mint-to')
+  .description('Mints tokens to wallet')
+  .option('--mint <value>', 'Mint address')
+  .option('--wallet <value>', 'Wallet address')
+  .option('--amount <value>', 'Mint amount')
+  .action(mintTo);
 const initializeProtocolCmd = program
   .command('initialize-protocol')
-  .description('Initializes protocol with taker and maker fees')
+  .description('Initializes protocol')
   .option('--maker-fee <value>', 'Maker fee')
   .option('--taker-fee <value>', 'Taker fee')
-  .option('--collateral-mint <value>', 'Collaterl mint public key')
+  .option('--collateral-mint <value>', 'Collateral mint address')
   .action(initializeProtocol);
 
-addDefaultArgs(initializeProtocolCmd);
-addDefaultArgs(setupMintsCmd);
 addDefaultArgs(airdropCmd);
+addDefaultArgs(createMintCmd);
+addDefaultArgs(createWalletCmd);
+addDefaultArgs(mintToCmd);
+addDefaultArgs(initializeProtocolCmd);
 
 /// EXECUTE
 
