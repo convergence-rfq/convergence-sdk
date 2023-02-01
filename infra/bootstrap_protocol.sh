4f4307b694ff971e29005d4ed3677370a616ffd6
Pindaroso <me@pindaroso.com>
2023-02-01 16:55:57 -0500

    Added devnet URL

--- infra/bootstrap_protocol.sh
+++ infra/bootstrap_protocol.sh
@@ -2,18 +2,19 @@
 
 set -e
 
-RPC_ENDPOINT="http://127.0.0.1:8899"
+#RPC_ENDPOINT="http://127.0.0.1:8899"
+RPC_ENDPOINT="https://api.devnet.solana.com"
 KEYPAIR_FILE="/Users/pindaroso/.config/solana/dao.json"
 
 SPOT_INSTRUMENT_PROGRAM_ID=FTzYU9cFnziCQvJ6ZUve7asaYqRexk7pBcn8zQ9MqoMQ
 PSYOPTIONS_EUROPEAN_INSTRUMENT_PROGRAM_ID=BeJcAotUcFEWXVnh6qaBBLKaZaoaM8HSFY1L3sb1Aps3
 PSYOPTIONS_AMERICAN_INSTRUMENT_PROGRAM_ID=Cdaz7EGr2AVDkCpjMVEXDzap6oRrrrZUEaKAVLmJAX7R
 
-CVG=convergence
-#CVG="node /Users/pindaroso/code/convergence-sdk/packages/cli/dist/cjs/index.cjs"
+#CVG=convergence
+CVG="node /Users/pindaroso/code/convergence-sdk/packages/cli/dist/cjs/index.cjs"
 OPT="--rpc-endpoint=$RPC_ENDPOINT --keypair-file=$KEYPAIR_FILE"
 
-$CVG airdrop
+$CVG airdrop $OPT
 read COLLATERAL_MINT_ADDRESS <<< $($CVG create-mint $OPT --decimals=6 | awk '/Address:[[:space:]]/ { print $2 }')
 read BASE_ASSET_MINT_ADDRESS <<< $($CVG create-mint $OPT --decimals=9 | awk '/Address:[[:space:]]/ { print $2 }')
 $CVG initialize-protocol $OPT --taker-fee=1 --maker-fee=0 --collateral-mint=$COLLATERAL_MINT_ADDRESS
