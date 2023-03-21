5ab8a8ae1df938f993317c94b0e479150032f68a
Pindaroso <me@pindaroso.com>
2023-03-21 11:08:06 -0400

    Refactoring

--- packages/js/tests/unit/rfq.spec.ts
+++ packages/js/tests/unit/rfq.spec.ts
@@ -8,15 +8,48 @@ import {
   spawnValidator,
 } from '../../../validator';
 import { createSdk } from '../helpers';
-import { OrderType, SpotInstrument, Side } from '../../src';
+import { OrderType, SpotInstrument, Side, Mint } from '../../src';
 
 describe('rfq', () => {
   let validator: ChildProccess;
   let ctx: Ctx;
 
+  let baseMint: Mint;
+  let quoteMint: Mint;
+
+  const setMints = (done: any) => {
+    let counter = 2;
+
+    const tryDone = () => {
+      counter -= 1;
+      if (counter === 0) {
+        done();
+      }
+    };
+
+    createSdk('dao').then((cvg) => {
+      cvg
+        .tokens()
+        .findMintByAddress({ address: new PublicKey(ctx.baseMint) })
+        .then((mint) => {
+          baseMint = mint;
+          tryDone();
+        });
+      cvg
+        .tokens()
+        .findMintByAddress({ address: new PublicKey(ctx.quoteMint) })
+        .then((mint) => {
+          quoteMint = mint;
+          tryDone();
+        });
+    });
+  };
+
   before((done) => {
     ctx = readCtx();
-    validator = spawnValidator(done);
+
+    // Validator takes a callback so if we need to set data like mints do it here
+    validator = spawnValidator(() => setMints(done));
   });
 
   after(() => {
@@ -25,11 +58,7 @@ describe('rfq', () => {
 
   it('create', async () => {
     const cvg = await createSdk('taker');
-    const [baseMint, quoteMint] = await Promise.all([
-      cvg.tokens().findMintByAddress({ address: new PublicKey(ctx.baseMint) }),
-      cvg.tokens().findMintByAddress({ address: new PublicKey(ctx.quoteMint) }),
-    ]);
-    const { rfq } = await cvg.rfqs().createAndFinalize({
+    const { rfq, response } = await cvg.rfqs().createAndFinalize({
       instruments: [
         new SpotInstrument(cvg, baseMint, {
           amount: 1.5,
@@ -38,12 +67,11 @@ describe('rfq', () => {
       ],
       orderType: OrderType.TwoWay,
       fixedSize: { __kind: 'BaseAsset', legsMultiplierBps: 1 },
-      quoteAsset: cvg
-        .instrument(new SpotInstrument(cvg, quoteMint))
-        .toQuoteAsset(),
+      quoteAsset: new SpotInstrument(cvg, quoteMint).toQuoteAsset(),
       activeWindow: 60 * 60,
       settlingWindow: 50 * 5,
     });
     expect(rfq).toHaveProperty('address');
+    expect(response.signature).toBeDefined();
   });
 });
