54a39d0e3250d93adb27bdf1a574831687724d6c
Pindaroso <me@pindaroso.com>
2023-05-01 18:33:05 -0400

    More tests

--- packages/cli/tests/unit/riskEngine.spec.ts
+++ packages/cli/tests/unit/riskEngine.spec.ts
@@ -72,4 +72,64 @@ describe('riskEngine', () => {
     ]);
     expect(stub.args[0][0]).toEqual(TX_LABEL);
   });
+
+  it('set-risk-categories-info [very low]', async () => {
+    await runCli([
+      'risk-engine',
+      'set-risk-categories-info',
+      '--category',
+      'very-low',
+      '--new-value',
+      '0.05,0.5,0.02,0.2,0.04,0.3,0.08,0.4,0.12,0.5,0.2,0.6,0.3,0.7',
+    ]);
+    expect(stub.args[0][0]).toEqual(TX_LABEL);
+  });
+
+  it('set-risk-categories-info [low]', async () => {
+    await runCli([
+      'risk-engine',
+      'set-risk-categories-info',
+      '--category',
+      'low',
+      '--new-value',
+      '0.05,0.8,0.04,0.4,0.08,0.6,0.16,0.8,0.24,1.0,0.4,1.2,0.6,1.4',
+    ]);
+    expect(stub.args[0][0]).toEqual(TX_LABEL);
+  });
+
+  it('set-risk-categories-info [medium]', async () => {
+    await runCli([
+      'risk-engine',
+      'set-risk-categories-info',
+      '--category',
+      'medium',
+      '--new-value',
+      '0.05,1.2,0.06,0.6,0.12,0.9,0.24,1.2,0.36,1.5,0.6,1.8,0.9,2.1',
+    ]);
+    expect(stub.args[0][0]).toEqual(TX_LABEL);
+  });
+
+  it('set-risk-categories-info [high]', async () => {
+    await runCli([
+      'risk-engine',
+      'set-risk-categories-info',
+      '--category',
+      'high',
+      '--new-value',
+      '0.05,2.4,0.08,0.8,0.16,1.2,0.32,1.6,0.48,2.0,0.8,2.4,1.2,2.8',
+    ]);
+    expect(stub.args[0][0]).toEqual(TX_LABEL);
+  });
+
+  it('set-risk-categories-info [very high]', async () => {
+    await runCli([
+      'risk-engine',
+      'set-risk-categories-info',
+      '--category',
+      'very-high',
+      '--new-value',
+      '0.05,5.0,0.10,1.0,0.20,1.5,0.40,2.0,0.60,2.5,1.0,3.0,1.5,3.5',
+    ]);
+    expect(stub.args[0][0]).toEqual(TX_LABEL);
+  });
 });
