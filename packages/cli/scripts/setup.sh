00db9a11d1b79b92769f4ac471b3dac3737fddef
Pindaroso <me@pindaroso.com>
2023-05-02 08:47:59 -0400

    Refactoring

--- packages/cli/scripts/setup.sh
+++ packages/cli/scripts/setup.sh
@@ -4,19 +4,16 @@ set -e
 
 source .env
 
-KEYPAIR_FILE="$HOME/.config/solana/mint-authority.json"
-OPTS="--rpc-endpoint=$RPC_ENDPOINT --keypair-file=$KEYPAIR_FILE"
-
 convergence airdrop sol
 
-read USDC_MINT <<< $(convergence token create-mint $OPTS --decimals=6 | awk '/Address:[[:space:]]/ { print $2 }')
-read BTC_MINT  <<< $(convergence token create-mint $OPTS --decimals=9 | awk '/Address:[[:space:]]/ { print $2 }')
-read SOL_MINT  <<< $(convergence token create-mint $OPTS --decimals=9 | awk '/Address:[[:space:]]/ { print $2 }')
+read USDC_MINT <<< $(convergence token create-mint --decimals=6 | awk '/Address:[[:space:]]/ { print $2 }')
+read BTC_MINT  <<< $(convergence token create-mint --decimals=9 | awk '/Address:[[:space:]]/ { print $2 }')
+read SOL_MINT  <<< $(convergence token create-mint --decimals=9 | awk '/Address:[[:space:]]/ { print $2 }')
 
-read USDC_WALLET <<< $(convergence token create-wallet $OPTS --mint=$USDC_MINT --owner=$OWNER | awk '/Address:[[:space:]]/ { print $2 }')
-read BTC_WALLET  <<< $(convergence token create-wallet $OPTS --mint=$BTC_MINT  --owner=$OWNER | awk '/Address:[[:space:]]/ { print $2 }')
-read SOL_WALLET  <<< $(convergence token create-wallet $OPTS --mint=$SOL_MINT  --owner=$OWNER | awk '/Address:[[:space:]]/ { print $2 }')
+read USDC_WALLET <<< $(convergence token create-wallet --mint=$USDC_MINT --owner=$OWNER | awk '/Address:[[:space:]]/ { print $2 }')
+read BTC_WALLET  <<< $(convergence token create-wallet --mint=$BTC_MINT  --owner=$OWNER | awk '/Address:[[:space:]]/ { print $2 }')
+read SOL_WALLET  <<< $(convergence token create-wallet --mint=$SOL_MINT  --owner=$OWNER | awk '/Address:[[:space:]]/ { print $2 }')
 
-convergence token mint-to $OPTS --mint=$USDC_MINT --wallet=$USDC_WALLET --amount=1000000000000000
-convergence token mint-to $OPTS --mint=$BTC_MINT  --wallet=$BTC_WALLET  --amount=1000000000000000
-convergence token mint-to $OPTS --mint=$SOL_MINT  --wallet=$SOL_WALLET  --amount=1000000000000000
+convergence token mint-to --mint=$USDC_MINT --wallet=$USDC_WALLET --amount=1000000000000000
+convergence token mint-to --mint=$BTC_MINT  --wallet=$BTC_WALLET  --amount=1000000000000000
+convergence token mint-to --mint=$SOL_MINT  --wallet=$SOL_WALLET  --amount=1000000000000000
