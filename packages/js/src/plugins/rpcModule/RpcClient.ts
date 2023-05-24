54965c1d3643db1753628ae73f7b3978ca3945ee
Nagaprasadvr <nagaprasadvr246@gmail.com>
2023-05-24 15:13:28 +0530

    add confirm transaction

--- packages/js/src/plugins/rpcModule/RpcClient.ts
+++ packages/js/src/plugins/rpcModule/RpcClient.ts
@@ -156,14 +156,30 @@ export class RpcClient {
 
   async serializeAndSendTransaction(
     transaction: Transaction,
-    sendOptions: SendOptions = {}
-  ): Promise<TransactionSignature> {
+    confirmOptions?: ConfirmOptions
+  ): Promise<SendAndConfirmTransactionResponse> {
+    const blockhashWithExpiryBlockHeight = {
+      blockhash: transaction.recentBlockhash as string,
+      lastValidBlockHeight: transaction.lastValidBlockHeight as number,
+    };
     const rawTransaction = transaction.serialize();
+    const sendOptions: SendOptions = {
+      skipPreflight: confirmOptions?.skipPreflight,
+      preflightCommitment: confirmOptions?.preflightCommitment,
+      maxRetries: confirmOptions?.maxRetries,
+      minContextSlot: confirmOptions?.minContextSlot,
+    };
     try {
-      return await this.convergence.connection.sendRawTransaction(
+      const signature = await this.convergence.connection.sendRawTransaction(
         rawTransaction,
         sendOptions
       );
+      const confirmResponse = await this.confirmTransaction(
+        signature,
+        blockhashWithExpiryBlockHeight,
+        confirmOptions?.commitment
+      );
+      return { signature, confirmResponse, ...blockhashWithExpiryBlockHeight };
     } catch (error) {
       throw this.parseProgramError(error, transaction);
     }
