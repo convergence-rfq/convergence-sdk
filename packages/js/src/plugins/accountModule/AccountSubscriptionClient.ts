b21ad28296550c480ff1011bc7cb284837773287
Dane Wilson <git@danewilson.me>
2023-06-06 15:04:47 -0400

    add methods

--- packages/js/src/plugins/accountModule/AccountSubscriptionClient.ts
+++ packages/js/src/plugins/accountModule/AccountSubscriptionClient.ts
@@ -1,5 +1,21 @@
-import type { Convergence } from '../../Convergence';
+import { AccountInfo } from "@solana/web3.js";
+import type { Convergence, PublicKey } from '../..';
+
+type AccountChangeListener = (accountInfo: AccountInfo<Buffer>) => void;
 
 export class AccountSubscriptionClient {
   constructor(protected readonly convergence: Convergence) {}
+
+  subscribe(account: PublicKey, listener: AccountChangeListener) {
+    const subscriptionId = this.convergence.connection.onAccountChange(
+      account, 
+      listener,
+      "confirmed"
+    );
+    return subscriptionId;
+  }
+
+  unsubscribe(subscriptionId: number) {
+    return this.convergence.connection.removeAccountChangeListener(subscriptionId);
+  }
 }
