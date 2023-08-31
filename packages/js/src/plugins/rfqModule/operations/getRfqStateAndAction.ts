bdf66850281b0e10a6211ebde31274e92129b89e
Nagaprasadvr <nagaprasadvr246@gmail.com>
2023-08-31 16:38:16 +0530

    add rfqStateValidUntil field

--- packages/js/src/plugins/rfqModule/operations/getRfqStateAndAction.ts
+++ packages/js/src/plugins/rfqModule/operations/getRfqStateAndAction.ts
@@ -2,7 +2,15 @@ import { Rfq } from '../models';
 import { Operation, SyncOperationHandler, useOperation } from '../../../types';
 const Key = 'GetRfqStateAndAction' as const;
 
-export type RfqState = 'Active' | 'Cancelled' | 'Constructed';
+export type RfqState =
+  | 'Active'
+  | 'Cancelled'
+  | 'Constructed'
+  | 'Expired'
+  | 'Settling'
+  | 'SettlingEnded'
+  | null;
+
 export type RfqAction =
   | 'Cancel'
   | 'UnlockCollateral'
@@ -53,6 +61,7 @@ export type GetRfqStateAndActionInput = {
 export type GetRfqStateAndActionOutput = {
   rfqState: RfqState;
   rfqAction: RfqAction;
+  rfqStateValidUntil: Date | null;
 };
 
 /**
@@ -64,18 +73,53 @@ export const getRfqStateAndActionHandler: SyncOperationHandler<GetRfqStateAndAct
     handle: (operation: GetRfqStateAndAction): GetRfqStateAndActionOutput => {
       const { rfq, caller } = operation.input;
       const rfqState = getRfqState(rfq);
+      const rfqStateValidUntil = getRfqStateValidaity(rfqState, rfq);
       const rfqAction = getRfqAction(rfq, rfqState, caller);
-      return { rfqState, rfqAction };
+      return { rfqState, rfqStateValidUntil, rfqAction };
     },
   };
 
-const getRfqState = (rfq: Rfq): RfqState => {
-  if (rfq.state === 'active') {
-    return 'Active';
-  } else if (rfq.state === 'canceled') {
-    return 'Cancelled';
+const getRfqStateValidaity = (rfqState: RfqState, rfq: Rfq) => {
+  if (rfqState === 'Active') {
+    const timestampStart = new Date(Number(rfq.creationTimestamp));
+    const timestampExpiry = new Date(
+      timestampStart.getTime() + Number(rfq.activeWindow) * 1000
+    );
+    return timestampExpiry;
   }
-  return 'Constructed';
+
+  if (rfqState === 'Settling') {
+    const timestampStart = new Date(Number(rfq.creationTimestamp));
+    const timestampExpiry = new Date(
+      timestampStart.getTime() + Number(rfq.activeWindow) * 1000
+    );
+    const timestampSettlement = new Date(
+      timestampExpiry.getTime() + Number(rfq.settlingWindow) * 1000
+    );
+    return timestampSettlement;
+  }
+  return null;
+};
+
+const getRfqState = (rfq: Rfq): RfqState => {
+  const timestampStart = new Date(Number(rfq.creationTimestamp));
+  const timestampExpiry = new Date(
+    timestampStart.getTime() + Number(rfq.activeWindow) * 1000
+  );
+  const rfqExpired = timestampExpiry.getTime() <= Date.now();
+  const timestampSettlement = new Date(
+    timestampExpiry.getTime() + Number(rfq.settlingWindow) * 1000
+  );
+  const settlementWindowElapsed = timestampSettlement.getTime() <= Date.now();
+  if (rfq.state === 'constructed') return 'Constructed';
+  if (rfq.state === 'canceled') return 'Cancelled';
+  if (rfq.state === 'active') {
+    if (!rfqExpired) return 'Active';
+    if (rfqExpired && rfq.confirmedResponses === 0) return 'Expired';
+    if (!settlementWindowElapsed) return 'Settling';
+    if (settlementWindowElapsed) return 'SettlingEnded';
+  }
+  return null;
 };
 
 const getRfqAction = (
