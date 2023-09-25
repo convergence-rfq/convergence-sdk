f18799402d7c60bb71d1dddf6b295c4c5e2cd1de
Nagaprasadvr <nagaprasadvr246@gmail.com>
2023-09-25 12:15:45 +0530

    remove redundant methods

--- packages/js/src/plugins/instrumentModule/methods.ts
+++ packages/js/src/plugins/instrumentModule/methods.ts
@@ -1,6 +1,5 @@
 import { QuoteAsset, legBeet } from '@convergence-rfq/rfq';
 
-import { AccountMeta } from '@solana/web3.js';
 import { createSerializerFromFixableBeetArgsStruct } from '../../types';
 import { LegInstrument, QuoteInstrument } from './types';
 
@@ -11,14 +10,6 @@ export function serializeAsLeg(legInstrument: LegInstrument) {
 
 export function getSerializedLegLength(legInstrument: LegInstrument) {
   return serializeAsLeg(legInstrument).length;
-}
-
-export function getProgramAccount(legInstrument: LegInstrument): AccountMeta {
-  return {
-    pubkey: legInstrument.getProgramId(),
-    isSigner: false,
-    isWritable: false,
-  };
 }
 
 export function toQuote(legInstrument: QuoteInstrument): QuoteAsset {
