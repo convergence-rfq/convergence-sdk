499141776fc8e129e8cdc57c9ddbf3fcba0b04ac
Yaroslav Khodakovskij <wwwinche@gmail.com>
2023-11-27 14:06:21 +0200

    Integrate CPL changes with added upgradability to accounts

--- packages/js/src/plugins/instrumentModule/methods.ts
+++ packages/js/src/plugins/instrumentModule/methods.ts
@@ -1,4 +1,4 @@
-import { Leg, QuoteAsset, legBeet } from '@convergence-rfq/rfq';
+import { ApiLeg, QuoteAsset, legBeet } from '@convergence-rfq/rfq';
 
 import { AccountMeta } from '@solana/web3.js';
 import { createSerializerFromFixableBeetArgsStruct } from '../../types';
@@ -10,7 +10,7 @@ import { SpotLegInstrument } from '../spotInstrumentModule';
 import { LegInstrument, QuoteInstrument } from './types';
 import { Convergence } from '@/Convergence';
 
-export function toLeg(legInstrument: LegInstrument): Leg {
+export function toLeg(legInstrument: LegInstrument): ApiLeg {
   return {
     instrumentProgram: legInstrument.getProgramId(),
     baseAssetIndex: legInstrument.getBaseAssetIndex(),
@@ -26,7 +26,10 @@ export function toLeg(legInstrument: LegInstrument): Leg {
 
 export function serializeAsLeg(legInstrument: LegInstrument) {
   const legSerializer = createSerializerFromFixableBeetArgsStruct(legBeet);
-  return legSerializer.serialize(toLeg(legInstrument));
+  return legSerializer.serialize({
+    ...toLeg(legInstrument),
+    reserved: new Array(64).fill(0),
+  });
 }
 
 export function getSerializedLegLength(legInstrument: LegInstrument) {
